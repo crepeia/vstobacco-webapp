ee64bd104dde169bf9f1e2ec1deb8dfd2bb82b7a
Luciana Nascimento <luciananssantana@gmail.com>
2020-10-12 09:52:16 -0300

    Criação das telas
    Criação dos arquivos das telas

---
+++ src/navigation/MainNavigator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//telas
+import AboutScreen from '../screens/AboutScreen';
+import AddCigarrosScreen from '../screens/AddCigarros';
+import DesafiosScreen from '../screens/DesafiosScreen';
+import DicasScreen from '../screens/DicasScreen';
+import HomeScreen from '../screens/HomeScreen';
+import LifeAndMoneyScreen from '../screens/LifeAndMoneyScreen';
+import LoginScreen from '../screens/LoginScreen';
+import OptionsScreen from '../screens/OptionsScreen';
+import PerfilScreen from '../screens/PerfilScreen';
+import RankingScreen from '../screens/RankingScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
